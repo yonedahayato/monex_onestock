fda18aa1600295bd7462874dd36eab74375b18ba
yoneda hayato <yhayato1320@gmail.com>
2017-09-10 11:07:15 +0900

    fix no data error

--- calculat_profit_rate.py
+++ calculat_profit_rate.py
@@ -5,32 +5,46 @@ import pandas as pd
 
 def calculate_profit_rate(rate="profit_rate"):
     today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=1)
-    day_before_yesterday = today - datetime.timedelta(days=2)
+    while True:
+        try:
+            data_1 = get_data_AfternoonSession(str(today))
+            break
+        except Exception as e:
+            print(str(today)+" is error: {}".format(e))
+            today = today - datetime.timedelta(days=1)
 
-    data_1 = get_data_AfternoonSession(str(yesterday))
-    data_2 = get_data_AfternoonSession(str(day_before_yesterday))
+    yesterday = today - datetime.timedelta(days=1)
+    while True:
+        try:
+            data_2 = get_data_AfternoonSession(str(yesterday))
+            break
+        except Exception as e:
+            print(str(yesterday)+" is error: {}".format(e))
+            yesterday = yesterday - datetime.timedelta(days=1)
 
     data_1 = data_1.ix[:, ["コード", "銘柄名", "始値"]]
     data_2 = data_2.ix[:, ["コード", "銘柄名", "始値"]]
 
-    data_1 = data_1.rename(columns={"始値": "始値_"+str(yesterday)})
-    data_2 = data_2.rename(columns={"始値": "始値_"+str(day_before_yesterday)})
+    data_1 = data_1.rename(columns={"始値": "始値_"+str(today)})
+    data_2 = data_2.rename(columns={"始値": "始値_"+str(yesterday)})
 
     data_1 = data_1.set_index("コード")
     data_2 = data_2.set_index("コード")
-    data = pd.concat([data_1, data_2.ix[:, "始値_"+str(day_before_yesterday)]], axis=1)
+    data = pd.concat([data_1, data_2.ix[:, "始値_"+str(yesterday)]], axis=1)
     data = data.dropna()
+    data.index = data.index.map(lambda x: x.replace("-T", ""))
 
-    data["diff"] = (data["始値_"+str(yesterday)]-data["始値_"+str(day_before_yesterday)])
-    data["commision"] = (data.ix[:, "始値_"+str(yesterday)].map(lambda x: max(x*0.005, 48)) + data.ix[:, "始値_"+str(day_before_yesterday)].map(lambda x: max(x*0.005, 48)))
+
+    data["diff"] = (data["始値_"+str(today)]-data["始値_"+str(yesterday)])
+    data["commision"] = (data.ix[:, "始値_"+str(today)].map(lambda x: max(x*0.005, 48)) + data.ix[:, "始値_"+str(yesterday)].map(lambda x: max(x*0.005, 48)))
+
     data["profit"] = data["diff"] - data["commision"]
-    data["profit_rate"] = data["profit"] / data["始値_"+str(day_before_yesterday)]
+    data["profit_rate"] = data["profit"] / data["始値_"+str(yesterday)]
+
     if rate=="profit":
         data = data.sort_values(by="profit", ascending=False)
     elif rate=="profit_rate":
         data = data.sort_values(by="profit_rate", ascending=False)
-
     return data
 
 if __name__ == "__main__":
--- get_stock_data.py
+++ get_stock_data.py
@@ -7,10 +7,13 @@ def get_data_AfternoonSession(date):
     csv_url = "http://k-db.com/stocks/"+str(date)+"/b?download=csv"
     res = request.urlopen(csv_url)
     data = res.read().decode("shift-jis")
+    if data == "":
+        raise Exception("that day has nodata")
+
     data_df = pd.read_csv(StringIO(data))
     data_df = data_df.ix[data_df["市場"]=="東証1部", :]
     data_df = data_df.reset_index(drop=True)
-    print("len(data_df): {}".format(len(data_df)))
+    print(str(date)+": len(data_df): {}".format(len(data_df)))
     return data_df
 
 if __name__ == "__main__":
