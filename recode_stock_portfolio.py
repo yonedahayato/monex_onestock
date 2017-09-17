d7c1f1bed0621acb658274904b349cce682d42af
yoneda hayato <yhayato1320@gmail.com>
2017-09-18 08:00:31 +0900

    record fix

--- recode_stock_portfolio.py
+++ recode_stock_portfolio.py
@@ -6,13 +6,12 @@ def recode_stock_portfolio(status, code, number, price="nan"):
     file_name = "./recode_portfolio.csv"
     if not path.exists(file_name):
         Path("./recode_portfolio.csv").touch()
+        recode_df = pd.DataFrame({"code": [code], "status": [status], "number": [number], "price": [price]})
     else:
         recode_df = pd.read_csv(file_name, header=1)
-
-    recode_df = pd.concat([recode_df, pd.DataFrame({"code": [code], "status": [status], "number": [number], "price": [price]})])
+        recode_df = pd.concat([recode_df, pd.DataFrame({"code": [code], "status": [status], "number": [number], "price": [price]})])
 
     recode_df.to_csv(file_name, index=False, header=True)
-
 
 if __name__ == "__main__":
     recode_stock_portfolio()
